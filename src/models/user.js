f9505bb5bf5ae04be3931bdbabff8699b26b869e
zo0o0oma <richzuxe333@gmail.com>
2021-02-12 20:00:48 +0200

    first commit

--- src/models/user.js
+++ src/models/user.js
@@ -7,11 +7,11 @@ const UserSchema = new mongoose.Schema(
     Confirmation: { type: String, require: true },
     role: { type: Number, defult: 0 },
     thumbnail: String,
+  },
+  {
     toJSON: {
       virtuals: true,
     },
-  },
-  {
     timestamps: true,
   }
 );
