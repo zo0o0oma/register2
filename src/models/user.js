7a245ab00c62179d254deec306cdde4d2bdbf03e
zo0o0oma <richzuxe333@gmail.com>
2021-02-12 19:52:46 +0200

    first commit

--- src/models/user.js
+++ src/models/user.js
@@ -1,20 +1,16 @@
 import mongoose from 'mongoose';
-const UserSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    Confirmation: { type: String, require: true },
-    role: { type: Number, defult: 0 },
-    thumbnail: String,
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  Confirmation: { type: String, require: true },
+  role: { type: Number, defult: 0 },
+  thumbnail: String,
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
   },
-  {
-    timestamps: true,
-    toJSON: {
-      virtuals: true,
-    },
-  }
-);
+});
 UserSchema.virtual('thumbnail_url').get(function () {
   return `http://localhost:3000/uploads/${this.thumbnail}`;
 });
